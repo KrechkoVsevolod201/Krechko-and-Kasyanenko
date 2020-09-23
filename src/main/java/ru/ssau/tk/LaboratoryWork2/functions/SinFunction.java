a4dfaf4440a869917d97c10cec5ade583995220d
Sharky_001 <71152134+KrechkoVsevolod201@users.noreply.github.com>
2020-09-23 21:50:37 +0400

    XY commit 4 task

---
+++ src/main/java/ru/ssau/tk/LaboratoryWork2/functions/SinFunction.java
@@ -0,0 +1,8 @@
+package ru.ssau.tk.LaboratoryWork2.functions;
+
+public class SinFunction implements MathFunction {
+    public double apply(double x) {
+        return java.lang.Math.sin(x);
+
+    }
+}
---
+++ src/main/java/ru/ssau/tk/LaboratoryWork2/functions/SqrtFunction.java
@@ -0,0 +1,8 @@
+package ru.ssau.tk.LaboratoryWork2.functions;
+
+public class SqrtFunction implements MathFunction {
+    public double apply(double x) {
+        return java.lang.Math.sqrt(x);
+
+    }
+}
