51f2f26ee756647f9885eeeccbc332b869ff439a
Sharky_001 <71152134+KrechkoVsevolod201@users.noreply.github.com>
2020-09-23 21:30:56 +0400

    Y commit 3 task

---
+++ src/main/java/ru/ssau/tk/LaboratoryWork2/functions/SqrFunction.java
@@ -0,0 +1,7 @@
+package ru.ssau.tk.LaboratoryWork2.functions;
+
+public class SqrFunction  implements MathFunction{
+    public double apply(double x) {
+        return java.lang.Math.pow(x, 2);
+    }
+}
