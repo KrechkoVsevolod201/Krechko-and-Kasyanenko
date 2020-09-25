2a69e304aa3a620dad4db4d3b8199743780115ed
NastyaKasyanenko <71152435+NastyaKasyanenko@users.noreply.github.com>
2020-09-25 09:30:27 +0300

    X commit 5 task

---
+++ src/main/java/ru/ssau/tk/LaboratoryWork2/functions/CompositeFunction.java
@@ -0,0 +1,15 @@
+package ru.ssau.tk.LaboratoryWork2.functions;
+
+public class CompositeFunction implements MathFunction {
+    private final MathFunction firstFunction;
+    private final MathFunction secondFunction;
+
+    public CompositeFunction(MathFunction firstFunction, MathFunction secondFunction) {
+        this.firstFunction = firstFunction;
+        this.secondFunction = secondFunction;
+    }
+
+    public double apply(double x) {
+        return secondFunction.apply(firstFunction.apply(x));
+    }
+}
