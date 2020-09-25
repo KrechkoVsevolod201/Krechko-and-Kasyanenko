adcb385825a97e7a3edb586e2e545b17666cbca5
NastyaKasyanenko <71152435+NastyaKasyanenko@users.noreply.github.com>
2020-09-25 09:48:06 +0300

    добавлены коментарии

--- src/main/java/ru/ssau/tk/LaboratoryWork2/functions/ConstantFunction.java
+++ src/main/java/ru/ssau/tk/LaboratoryWork2/functions/ConstantFunction.java
@@ -1,17 +1,17 @@
 package ru.ssau.tk.LaboratoryWork2.functions;
 
 public class ConstantFunction implements MathFunction {
-    private final double constant;
+    private final double constant;//инициализация переменной
 
     public double getConstant() {
-        return constant;
+        return constant;//возвращение переменной
     }
 
     public ConstantFunction(double constant) {
-        this.constant = constant;
+        this.constant = constant;//инициализация в конструкторе
     }
 
-    @Override
+    @Override//переопределение метода базового класса.
     public double apply(double x) {
         return constant;
     }
