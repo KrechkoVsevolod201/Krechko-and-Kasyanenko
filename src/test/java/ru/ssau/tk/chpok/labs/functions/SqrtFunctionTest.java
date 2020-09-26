73f3317b2aafaf94df52b358a1b76a2f5be4ed57
NastyaKasyanenko <71152435+NastyaKasyanenko@users.noreply.github.com>
2020-09-26 07:38:20 +0300

    добавлен 3 тест

---
+++ src/test/java/ru/ssau/tk/chpok/labs/functions/SqrtFunctionTest.java
@@ -0,0 +1,17 @@
+package ru.ssau.tk.chpok.labs.functions;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class SqrtFunctionTest {
+
+    @Test
+    public void testApply() {
+        SqrtFunction testFunction = new SqrtFunction();
+        assertEquals(testFunction.apply(1.0), 1,0);
+        assertNotEquals(testFunction.apply(5.0), 1,0);
+        assertEquals(testFunction.apply(4.0), 2.0,0);
+        assertEquals(testFunction.apply(144), 12,0);
+    }
+}
