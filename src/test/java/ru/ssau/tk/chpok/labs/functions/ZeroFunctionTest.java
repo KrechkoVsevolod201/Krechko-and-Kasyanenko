a5b13f6799f63baa1b7f1f1329ab890ac3c3c9c5
NastyaKasyanenko <71152435+NastyaKasyanenko@users.noreply.github.com>
2020-09-26 08:13:15 +0300

    добавлен 8 тест

---
+++ src/test/java/ru/ssau/tk/chpok/labs/functions/ZeroFunctionTest.java
@@ -0,0 +1,21 @@
+package ru.ssau.tk.chpok.labs.functions;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class ZeroFunctionTest {
+    ZeroFunction testFunction = new ZeroFunction();
+    @Test
+    public void testGetConstant(){
+        assertEquals(testFunction.apply(0), 0.0,0);
+        assertEquals(testFunction.apply(-8876542), 0.0,0);
+        assertEquals(testFunction.apply(742), 0.0,0);
+    }
+    @Test
+    public void testApply() {
+        assertEquals(testFunction.getConstant(), 0.0, 0);
+
+    }
+
+}
