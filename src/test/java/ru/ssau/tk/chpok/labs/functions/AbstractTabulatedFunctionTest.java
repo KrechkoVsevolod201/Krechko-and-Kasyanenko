7e576e2078c1840e51636ebb8ddec7a7e24865a7
Sharky_001 <71152134+KrechkoVsevolod201@users.noreply.github.com>
2020-10-10 10:29:18 +0400

    и ещё

--- src/test/java/ru/ssau/tk/chpok/labs/functions/AbstractTabulatedFunctionTest.java
+++ src/test/java/ru/ssau/tk/chpok/labs/functions/AbstractTabulatedFunctionTest.java
@@ -1,74 +1,34 @@
 package ru.ssau.tk.chpok.labs.functions;
 
-import org.testng.annotations.Test;
+abstract class AbstractTabulatedFunction implements TabulatedFunction {
 
-import static org.testng.Assert.*;
+    protected abstract int floorIndexOfX(double x);
 
-public class AbstractTabulatedFunctionTest {
-    MockTabulatedFunction newMock = new MockTabulatedFunction();
-    private static double delta = 0.001;
-    //expression of the extrapolation is 3+x
-    @Test
-    public void testApply() {
-        assertEquals(newMock.apply(1), 4, delta);//less than leftBound
-        assertEquals(newMock.apply(5), 8, delta);//larger than RightBound
-        assertEquals(newMock.apply(6), 9, delta);
+    protected abstract double extrapolateLeft(double x);
+
+    protected abstract double extrapolateRight(double x);
+
+    protected abstract double interpolate(double x, int floorIndex);
+
+    protected double interpolate(double x, double leftX, double rightX, double leftY, double rightY) {
+        return (leftY + (x - leftX) * (rightY - leftY) / (rightX - leftX));
     }
 
-    @Test
-    public void testGetX() {
-        assertEquals(newMock.getX(0),2,delta);
-        assertEquals(newMock.getX(1),2,delta);
-        assertEquals(newMock.getX(2),2,delta);
+    @Override
+    public double apply(double x) {
+        if (x < leftBound()) {
+            return (extrapolateLeft(x));
+        }
+
+        if (x > rightBound()) {
+            return (extrapolateRight(x));
+        }
+
+        if (indexOfX(x) != -1) {
+            return getY(indexOfX(x));
+        }
+        else {
+            return interpolate(x, floorIndexOfX(x));
+        }
     }
-
-
-    @Test
-    public void testGetY() {
-    }
-
-    @Test
-    public void testSetY() {
-    }
-
-    @Test
-    public void testIndexOfX() {
-    }
-
-    @Test
-    public void testIndexOfY() {
-    }
-
-    @Test
-    public void testLeftBound() {
-    }
-
-    @Test
-    public void testRightBound() {
-    }
-
-    @Test
-    public void testGetCount() {
-    }
-
-    @Test
-    public void testFloorIndexOfX() {
-    }
-
-    @Test
-    public void testExtrapolateLeft() {
-    }
-
-    @Test
-    public void testExtrapolateRight() {
-    }
-
-    @Test
-    public void testInterpolate() {
-    }
-
-    @Test
-    public void testTestInterpolate() {
-    }
-
 }
