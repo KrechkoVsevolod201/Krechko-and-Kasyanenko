b845b6e6c9ec394fb010f8d633f065d54fd1054e
Sharky_001 <71152134+KrechkoVsevolod201@users.noreply.github.com>
2020-11-05 22:22:33 +0400

    Мы сделоли что-то непонятное...

---
+++ src/main/java/ru/ssau/tk/chpok/labs/io/FunctionsIO.java
@@ -0,0 +1,10 @@
+package ru.ssau.tk.chpok.labs.io;
+import ru.ssau.tk.chpok.labs.functions.factory.TabulatedFunctionFactory;
+
+import java.io.*;
+
+final class FunctionsIO {
+    static void readTabulatedFunction(BufferedInputStream inputStream, TabulatedFunctionFactory factory) {
+        throw new UnsupportedOperationException("Oh no, boy, I think that is wrong operator");
+    }
+}
