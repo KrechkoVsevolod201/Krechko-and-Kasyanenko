326d2f8e7cb417478ecd6fec3f37ef26f446ff22
Sharky_001 <71152134+KrechkoVsevolod201@users.noreply.github.com>
2020-11-19 22:14:40 +0400

    Добавления и поправочки

--- src/main/java/ru/ssau/tk/chpok/labs/concurrent/AddingMultiplyingTaskExecutor.java
+++ src/main/java/ru/ssau/tk/chpok/labs/concurrent/AddingMultiplyingTaskExecutor.java
@@ -4,9 +4,27 @@ import ru.ssau.tk.chpok.labs.functions.ConstantFunction;
 import ru.ssau.tk.chpok.labs.functions.LinkedListTabulatedFunction;
 import ru.ssau.tk.chpok.labs.functions.TabulatedFunction;
 
+
 public class AddingMultiplyingTaskExecutor {
     public static void main(String[] args) {
         TabulatedFunction function = new LinkedListTabulatedFunction(new ConstantFunction(2), 1, 100, 2);
 
+        AddingTask addingTask = new AddingTask(function);
+        MultiplyingTask multiplyingTask = new MultiplyingTask(function);
+
+        Thread thread1 = new Thread(multiplyingTask);
+        thread1.start();
+        Thread thread2 = new Thread(multiplyingTask);
+        thread2.start();
+        Thread thread3 = new Thread(addingTask);
+        thread3.start();
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(function.toString());
     }
 }
