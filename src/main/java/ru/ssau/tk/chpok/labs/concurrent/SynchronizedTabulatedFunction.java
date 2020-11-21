72841fa27ee8928a1a4e431715d3a5050d471526
NastyaKasyanenko <71152435+NastyaKasyanenko@users.noreply.github.com>
2020-11-21 19:47:00 +0300

    сделали 1 задание 2 задания 4 лабораторного задания

---
+++ src/main/java/ru/ssau/tk/chpok/labs/concurrent/SynchronizedTabulatedFunction.java
@@ -0,0 +1,111 @@
+package ru.ssau.tk.chpok.labs.concurrent;
+
+import ru.ssau.tk.chpok.labs.functions.Point;
+import ru.ssau.tk.chpok.labs.functions.TabulatedFunction;
+import ru.ssau.tk.chpok.labs.operations.TabulatedFunctionOperationService;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class SynchronizedTabulatedFunction implements TabulatedFunction {
+private final TabulatedFunction tabulatedFunction;
+
+public SynchronizedTabulatedFunction(TabulatedFunction tabulatedFunction) {
+        this.tabulatedFunction = tabulatedFunction;
+        }
+
+public interface Operation<T> {
+    T apply(SynchronizedTabulatedFunction synchronizedTabulatedFunction);
+}
+
+    public <T> T doSynchronously(Operation<? extends T> operation) {
+        synchronized (tabulatedFunction) {
+            return operation.apply(this);
+        }
+    }
+
+
+    @Override
+    public int getCount() {
+        synchronized (tabulatedFunction) {
+            return tabulatedFunction.getCount();
+        }
+    }
+
+    @Override
+    public double getX(int index) {
+        synchronized (tabulatedFunction) {
+            return tabulatedFunction.getX(index);
+        }
+    }
+
+    @Override
+    public double getY(int index) {
+        synchronized (tabulatedFunction) {
+            return tabulatedFunction.getY(index);
+        }
+    }
+
+    @Override
+    public void setY(int index, double value) {
+        synchronized (tabulatedFunction) {
+            tabulatedFunction.setY(index, value);
+        }
+    }
+
+    @Override
+    public int indexOfX(double x) {
+        synchronized (tabulatedFunction) {
+            return tabulatedFunction.indexOfX(x);
+        }
+    }
+
+    @Override
+    public int indexOfY(double y) {
+        synchronized (tabulatedFunction) {
+            return tabulatedFunction.indexOfY(y);
+        }
+    }
+
+    @Override
+    public double leftBound() {
+        synchronized (tabulatedFunction) {
+            return tabulatedFunction.leftBound();
+        }
+    }
+
+    @Override
+    public double rightBound() {
+        synchronized (tabulatedFunction) {
+            return tabulatedFunction.rightBound();
+        }
+    }
+
+    @Override
+    public Iterator<Point> iterator() {
+        synchronized (tabulatedFunction) {
+            Point[] points = TabulatedFunctionOperationService.asPoints(tabulatedFunction);
+            return new Iterator<>() {
+                int i = 0;
+                @Override
+                public boolean hasNext() {
+                    return i < points.length;
+                }
+                @Override
+                public Point next() {
+                    if (!hasNext()) {
+                        throw new NoSuchElementException();
+                    }
+                    return points[i++];
+                }
+            };
+        }
+    }
+
+    @Override
+    public double apply(double x) {
+        synchronized (tabulatedFunction) {
+            return tabulatedFunction.apply(x);
+        }
+    }
+}
